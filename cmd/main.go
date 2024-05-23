8e809ec16e160eaafedff838d3fa2700019558ab
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-05-23 05:58:08 +0300

    fix: main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,40 +1,78 @@
 package main
 
 import (
+	"database/sql"
+	"errors"
+	"fmt"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/go-pg/pg/v10"
+	"github.com/golang-migrate/migrate"
+	"github.com/golang-migrate/migrate/database/postgres"
+	_ "github.com/golang-migrate/migrate/database/postgres"
+	_ "github.com/golang-migrate/migrate/source/file"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
 	"system-for-adding-and-reading-posts-and-comments/graph"
-	"system-for-adding-and-reading-posts-and-comments/innternal/repository"
+	"system-for-adding-and-reading-posts-and-comments/innternal/repository/inMemory"
+	postgres2 "system-for-adding-and-reading-posts-and-comments/innternal/repository/postgres"
 )
 
-func main() {
-	DB := pg.Connect(&pg.Options{
-		User:     "postgres",
-		Password: "postgres",
-		Database: "OzonContest",
-		Addr:     "localhost:5436",
-	})
+const (
+	host     = "ozon-contest"
+	port     = 5432
+	user     = "postgres"
+	password = "postgres"
+	dbname   = "OzonContest"
+)
 
-	defer DB.Close()
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Println("Error loading .env file")
 	}
+}
+func main() {
+	typeStorage, exists := os.LookupEnv("STORAGE")
+	if !exists {
+		log.Fatal("STORAGE environment variable not set")
+	}
+	var c graph.Config
+	if typeStorage == "in-memory" {
+		repo := inMemory.NewInMemoryRepository()
+		c = graph.Config{Resolvers: &graph.Resolver{Repository: repo}}
+	} else {
+		psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
-	repo := repository.NewRepository(DB)
-	c := graph.Config{Resolvers: &graph.Resolver{
-		Repository: repo}}
+		db, err := sql.Open("postgres", psqlInfo)
+		if err != nil {
+			log.Fatalf("connection failed: %s", err.Error())
+		}
+		defer db.Close()
+		driver, err := postgres.WithInstance(db, &postgres.Config{})
+		if err != nil {
+			log.Fatal(err)
+		}
+		migration, err := migrate.NewWithDatabaseInstance("file://database/migration",
+			dbname, driver)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if err := migration.Up(); err != nil {
+			if !errors.Is(err, migrate.ErrNoChange) {
+				log.Fatal(err)
+			}
+		}
 
+		repo := postgres2.NewRepository(db)
+		c = graph.Config{Resolvers: &graph.Resolver{Repository: repo}}
+	}
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(c))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", "8080")
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+"8080", nil))
 }
